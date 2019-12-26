9154f1c4df8971c7642bfe7e3cce834c03ce85aa
Bima Armedianto <bima.armedianto@gmai.com>
2019-12-26 12:06:24 +0700

    Fitur baru : Pengaturan Bahasa

--- app/src/main/java/com/example/profil_1718003/MainActivity.java
+++ app/src/main/java/com/example/profil_1718003/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.profil_1718003;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
 
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner;
+    String spinner_items[] = {"English", "Indonesia"};
+    ArrayAdapter<String>arrayAdapter;
+    TextView followers, likes, tags;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spiner);
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, spinner_items);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        followers = findViewById(R.id.followers);
+        likes = findViewById(R.id.likes);
+        tags = findViewById(R.id.tags);
+        if (item == "English"){
+            followers.setText("Followers");
+            likes.setText("Likes");
+            tags.setText("Tags");
+        }else{
+            followers.setText("Pengikut");
+            likes.setText("Suka");
+            tags.setText("Menandai");
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
